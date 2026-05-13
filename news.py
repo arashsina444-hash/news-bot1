64b045656d304dbaa6ff1a4cf8e18414021ce638
arashsina444-hash <arashsina444@gmail.com>
2026-05-13 17:02:59 +0330

    Update news.py

--- news.py
+++ news.py
@@ -4,11 +4,14 @@ RSS_URL = "https://feeds.feedburner.com/reuters/technologyNews"
 
 def get_news():
     feed = feedparser.parse(RSS_URL)
-    print("Latest Reuters Technology News:")
-    for entry in feed.entries[:5]:
-        print(entry.title)
-        print(entry.link)
-        print("-" * 30)
+
+    with open("news.txt", "w", encoding="utf-8") as f:
+        f.write("Latest Tech News\n\n")
+
+        for entry in feed.entries[:10]:
+            f.write(entry.title + "\n")
+            f.write(entry.link + "\n")
+            f.write("---------------------\n")
 
 if __name__ == "__main__":
     get_news()
